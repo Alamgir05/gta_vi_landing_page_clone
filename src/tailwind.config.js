d151b068b616f9fae18a4b4be0f2bfadc4c6cd15
Alamgir05 <rph.alamgir55@gmail.com>
2025-09-05 02:59:16 +0530

    first commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pink: '#ff69b4',
+      },
+    },
+  },
+  plugins: [],
+}
